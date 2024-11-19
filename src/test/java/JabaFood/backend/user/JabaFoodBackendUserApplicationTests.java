055921d3f6613f96fe995f1b187778cd8f1df727
Mileny Nakahara <milenygominha@gmail.com>
2024-11-18 22:26:25 -0300

    initial project

---
+++ src/test/java/JabaFood/backend/user/JabaFoodBackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package JabaFood.backend.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabaFoodBackendUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
