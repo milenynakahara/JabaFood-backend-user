8daa791e1bd7ecdc6223c409ddaa47af3de2eee9
villar <ribeiros@outlook.com>
2024-12-04 21:27:59 +0000

    Update project description and Swagger configuration
    - Change project description in pom.xml from "Food order app" to "Jaba Food App"
    - Modify SwaggerConfig to use application properties for API title and version
    - Add version and description properties to application.yml
    - Use @project.version@ and @project.description@ placeholders in application.yml

--- src/main/java/app/jaba/configurations/SwaggerConfig.java
+++ src/main/java/app/jaba/configurations/SwaggerConfig.java
@@ -3,18 +3,23 @@ package app.jaba.configurations;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${spring.application.description}")
+    private String description;
+    @Value("${spring.application.version}")
+    private String version;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Jaba Food Backend User API")
-                        .version("1.0")
-                        .description("API documentation for Jaba Food Backend User"));
+                        .title(description)
+                        .version(version));
     }
 
     @Bean
